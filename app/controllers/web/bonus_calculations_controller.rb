5dc97b29b208080f019a6ecedf82ca1272679930
Maxim Seliverstov <maxim.web.developer@gmail.com>
2015-06-08 10:03:40 +0300

    Add total result. Update locales

--- app/controllers/web/bonus_calculations_controller.rb
+++ app/controllers/web/bonus_calculations_controller.rb
@@ -6,12 +6,16 @@ class Web::BonusCalculationsController < Web::ApplicationController
   def create
     attrs = params[:bonus_calculation][:bonuses_attributes].values
     valid_bonuses = attrs.map{|e| Bonus.new(e) }.select{|e| BonusContract.new(e).validate }
+
+    @total_result = 0
+
     valid_bonuses.each do |b|
       result = ::CalcService.calc({
         calc_strategy: detect_calc_strategy(b.bonus_type),
         formula_params: CalcParamsBuilder.build_for(b.bonus_type, b.attributes)
       })
       b.result = (result == 0) ? nil : result
+      @total_result += b.result
     end
     @form = BonusCalculationForm.new(BonusCalculation.new({bonuses: valid_bonuses}))
   end
